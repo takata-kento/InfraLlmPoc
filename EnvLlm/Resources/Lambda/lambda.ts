c9fcde470051ee73c3cc4062ae3a3fa43fbeacbd
takata-kento <takata_kento@ims-sol.co.jp>
2024-07-06 13:39:19 +0900

    Lambda実行ログを出力するロググループを定義

--- EnvLlm/Resources/Lambda/lambda.ts
+++ EnvLlm/Resources/Lambda/lambda.ts
@@ -1,26 +1,28 @@
 import * as aws from "@pulumi/aws";
 import * as archive from "@pulumi/archive";
 import * as assert from "@pulumi/pulumi/asset";
+import { LogGroup } from "@pulumi/aws/cloudwatch";
+import { Function } from "@pulumi/aws/lambda";
 
 // Lambdaリソース作成クラス
 export class ResourceLambda {
     /**
      * 実行ロール
      */
-    readonly iamRole: string;
+    private readonly iamRole: string;
     /**
      * Lambda関数名
      */
-    readonly functionName: string;
+    private readonly functionName: string;
     /**
      * Lambdaソースコードファイル
      */
-    readonly codeFile: string;
+    private readonly codeFile: string;
     /**
      * Lambdaランタイム
      * 基本的にpythonのみを想定しているため固定
      */
-    readonly RUNTIME = "python3.7";
+    private readonly RUNTIME = "python3.7";
 
     /**
      * Lambdaリソースを作成するための初期化を行います。
@@ -30,19 +32,18 @@ export class ResourceLambda {
      */
     constructor(_iamRole: string, _functionName: string, _codeFile: string){
         this.iamRole = _iamRole;
-        this.functionName = _functionName;
-        this.codeFile = _codeFile;
+        this.functionName = _functionName; this.codeFile = _codeFile;
     }
 
     /**
      * Lambdaリソースを作成します。
-     * @param CloudWatchLogsにログを出力するかを指定(true:する/false:しない)
-     * @param CICD実行ロールにAssumeRoleするためのProvider
-     * @returns boolean型の結果を返します。(true:成功/false:失敗)
+     * @param _provider CICD実行ロールにAssumeRoleするためのProvider
+     * @returns Lambdaリソースを表すインスタンス
      */
-    public create(_loggingFlag: boolean, _provider: aws.Provider): boolean{
-        let isSuccess = false;
+    public create(_provider: aws.Provider): Function | null{
         let codeZip: Promise<archive.GetFileResult>;
+        let lambdaFunction: Function | null = null;
+        let logGroup: LogGroup;
         
         // Lambdaソースコードファイルをzip化します。
         try{
@@ -54,27 +55,44 @@ export class ResourceLambda {
         }catch(e){
             console.log("zipファイル作成中にエラーが発生しました。");
             console.log(e);
-            return isSuccess;
+            return lambdaFunction;
         }
+
+        // ログ出力用のCloudWatchLogs ロググループを作成します。
+        logGroup = this.createCloudWatchForLambda(_provider);
 
         // Lambdaリソースを作成します。
-        try{
-            new aws.lambda.Function(this.functionName,{
-                code: new assert.FileArchive("lambda.zip"),
-                name: this.functionName,
-                role: this.iamRole,
-                sourceCodeHash: codeZip.then(lambda => lambda.outputBase64sha256),
-                runtime: this.RUNTIME
-            },{
-                provider: _provider
-            })
-            isSuccess = true;
-        }catch(e){
-            console.log("Lambdaリソース作成中にエラーが発生しました。");
-            console.log(e);
-            return isSuccess;
-        }
+        lambdaFunction = new aws.lambda.Function(this.functionName,{
+            code: new assert.FileArchive("lambda.zip"),
+            name: this.functionName,
+            role: this.iamRole,
+            sourceCodeHash: codeZip.then(lambda => lambda.outputBase64sha256),
+            runtime: this.RUNTIME,
+            loggingConfig: {
+                logFormat: "Text",
+                systemLogLevel: "INFO"
+            }
+        },{
+            provider: _provider,
+            dependsOn: [
+                logGroup
+            ]
+        })
 
-        return isSuccess;
+        return lambdaFunction;
+    }
+
+    /**
+     * Lambdaリソース用のCloudWatchロググループを作成します。
+     * @param _provider CICD実行ロールにAssumeRoleするためのProvider
+     * @returns CloudWatchLogsのロググループリソースを表すインスタンス
+     */
+    private createCloudWatchForLambda(_provider: aws.Provider): LogGroup{
+        return new aws.cloudwatch.LogGroup(this.functionName,{
+            name: `/aws/lambda/${this.functionName}`,
+            retentionInDays: 14
+        },{
+            provider: _provider
+        });
     }
 }
