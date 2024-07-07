29f3e7fa364b2b5a7c26b15035974f986281bbe5
takata-kento <takata_kento@ims-sol.co.jp>
2024-07-07 13:40:34 +0900

    langchainの使用方法を修正

--- EnvLlm/Resources/Lambda/LambdaSrc/python/lambdaFunc.py
+++ EnvLlm/Resources/Lambda/LambdaSrc/python/lambdaFunc.py
@@ -1,5 +1,4 @@
-from langchain_community.llms import Bedrock
-from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain_aws import BedrockLLM
 
 prompt_en="QUESTION:How to cook Miso soup?"
 prompt_jp="質問：味噌汁の作り方を説明してください。"
@@ -11,14 +10,8 @@ def lambda_handler(event, context):
     bedrock = boto3.client('bedrock-runtime', region_name = "ap-northeast-1")
 
     # Titan Text G1 - Express
-    llm = Bedrock(
-        model_id="amazon.titan-text-express-v1",
-        client=bedrock,
-        model_kwargs={
-            "maxTokenCount":1000
-        },
-        region_name="ap-northeast-1",
-        streaming=True,
-        callbacks=[StreamingStdOutCallbackHandler()])
-    llm.predict(prompt_en)
-    llm.predict(prompt_jp)
+    llm = BedrockLLM(model_id="amazon.titan-text-express-v1",model_kwargs={"maxTokenCount":1000})
+    answer_en = llm.invoke(prompt_en)
+    print("Titan Text G1 - Express: " + answer_en + "\n")
+    answer_jp = llm.invoke(prompt_jp)
+    print("Titan Text G1 - Express: " + answer_jp + "\n")
