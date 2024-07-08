0b886af2c9eb79c2712a8a66b3788df365e50f0a
takata-kento <takata_kento@ims-sol.co.jp>
2024-07-09 00:03:35 +0900

    config.requireRegionを使用していたためエクセプションが発生していたのを修正。beforeにインスタンス作成を入れてしまっていたためdescribeでインスタンス利用できなかったのを修正。systemLogLevelはnullではなくundefinedと厳密等価演算するように修正。

--- EnvLlm/test/test_lambda.ts
+++ EnvLlm/test/test_lambda.ts
@@ -2,6 +2,8 @@ import * as pulumi from "@pulumi/pulumi";
 import { Function } from "@pulumi/aws/lambda";
 import "mocha";
 import * as fs from "fs";
+import { ResourceLambda } from "../Resources/Lambda/lambda";
+import * as aws from "@pulumi/aws";
 
 // pulumiランタイムのモックを作成
 pulumi.runtime.setMocks({
@@ -21,21 +23,22 @@ pulumi.runtime.setMocks({
 );
 
 describe("Infrastructure", function() {
-    let infra: typeof import("../Resources/Lambda/lambda");
-    let lambda_logconfig;
-
-    before(async function() {
-        infra = (await import("../Resources/Lambda/lambda"));
-        let lambda_logconfig = new infra.ResourceLambda("arn:aws:iam::123456789012:role/RoleCicdInfraLlmPoc","lambda_logconfig","lambdaFunc.zip");
-    })
+    let lambda_logconfig: ResourceLambda = new ResourceLambda("arn:aws:iam::123456789012:role/RoleCicdInfraLlmPoc","lambda_logconfig","lambdaFunc.zip");;
+    const provider = new aws.Provider("privileged", {
+        assumeRole: {
+            roleArn: "arn:aws:iam::123456789012:role/RoleCicdInfraLlmPoc",
+            sessionName: "PulumiSession",
+            externalId: "PulumiApplication",
+        },
+    });
 
     describe("#lambda_logconfig",function() {
         it("check log config", function(done) {
-            let lambda_resource : Function = lambda_logconfig.create();
-            pulumi.all([lambda_resource.urn, lambda_resource.loggingConfig]).apply(([urn, loggingConfig]) => {
-                if (!(loggingConfig.logFormat === "Text")){
+            let lambda_resource = lambda_logconfig.create(provider);
+            pulumi.all([lambda_resource?.urn, lambda_resource?.loggingConfig]).apply(([urn, loggingConfig]) => {
+                if (!(loggingConfig?.logFormat === "Text")){
                     done(new Error(`ログフォーマットがTextになっていません。urn -> ${urn}`));
-                }else if (!(loggingConfig.systemLogLevel === null)){
+                }else if (!(loggingConfig?.systemLogLevel === undefined)){
                     done(new Error(`システムログレベルが設定されてしまっています。ログフォーマット"Text"では指定できません。urn -> ${urn}`));
                 }else {
                     done();
